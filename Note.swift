d9531136334d8f67ec24279129d24988f5374244
KappaJohns <johnnychiu123@gmail.com>
2017-06-30 10:08:35 -0400

    Add some MakeSchool Notes

---
+++ Note.swift
@@ -0,0 +1,14 @@
+//
+//  Note.swift
+//  MakeSchoolNotes
+//
+//  Created by Make School on 6/29/17.
+//  Copyright © 2017 MakeSchool. All rights reserved.
+//
+
+import Foundation
+class Note {
+    var title = ""
+    var content = ""
+    var modificationTime = Date()
+}
